4c8a5a03345f2a4622037c7b388aa4e40018a043
藤井昌汰 <shota@fujiimasanoMacBook-Pro.local>
2025-12-29 14:07:58 +0900

    Add Basic Authentication

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    // 指定されたIDとパスワード
+    if (user === 'info@mlbl.co.jp' && pwd === 'qnqv6r0421anz') {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse('Auth Required.', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  });
+}
+
